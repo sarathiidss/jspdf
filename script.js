1b7bd23cc22d055745c5d6dc67a5610e90f6a4c3
sarathiidss <sarathi@idsoftsource.com>
2021-06-22 19:56:25 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function downloadPDFWithjsPDF() {
-  var doc = new jspdf.jsPDF('p', 'pt', 'a4');
-
-  doc.html(document.querySelector('#mytable'), {
-    callback: function(doc) {
-      doc.save('MLB World Series Winners.pdf');
-    },
-    margin: [60, 60, 60, 60],
-    x: 32,
-    y: 32
-  });
-}
-
 $(document).ready(function() {
-  //downloadPDFWithjsPDF();
+  $('#print').click(function() {
+    html2canvas(document.getElementById('mytable'), {
+      onrendered: function(canvas) {
+        var data = canvas.toDataURL();
+        var docDefinition = {
+          content: [
+            {
+              image: data,
+              width: 500
+            }
+          ]
+        };
+        pdfMake.createPdf(docDefinition).download('Table.pdf');
+      }
+    });
+  });
 });
